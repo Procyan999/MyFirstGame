87854e6208c7d8c4a99835816e4dfa42fb5d8865
Procyan999 <artin.biniek@ocsbstudent.ca>
2017-10-03 09:57:18 -0400

    fdfsfd

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    // Use this for initialization
+     void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+        Vector2 movement = new Vector2(moveHorizontal, 2.0f);
+        rb.AddForce(movement * 5);
+    }
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
