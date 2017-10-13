0cb11d63645df31bd25c135bf7a270af7b76f822
Procyan999 <artin.biniek@ocsbstudent.ca>
2017-10-13 09:18:25 -0400

    reset

--- Assets/player.cs
+++ Assets/player.cs
@@ -6,12 +6,12 @@ public class player : MonoBehaviour {
     private Rigidbody2D rb;
     private GameObject cool;
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
       
 	}
     private void FixedUpdate()
