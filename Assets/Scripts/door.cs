a26d8b6b6c13f8de2d310d825523c847f6d7a8d0
Procyan999 <artin.biniek@ocsbstudent.ca>
2017-10-14 22:43:41 -0400

    go

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour {
+    public int LevelToLoad;
+    //public Text text;
+ //  private void hi;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("a"))
+        {
+            Application.LoadLevel(LevelToLoad);
+        } 
+    }
+   void  OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("a"))
+        {
+            if (Input.GetKeyDown("E"))
+            {
+                Application.LoadLevel(LevelToLoad);
+            }
+            }
+    }
+     void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("a"))
+        {
+          
+        }
+    }
+}
